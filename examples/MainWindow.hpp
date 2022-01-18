50a9b9be018110eebe799cf24a89f36125d29950
dips42 <vgui@yandex.ru>
2022-01-18 14:31:35 +0700

    Some modifications

--- examples/MainWindow.hpp
+++ examples/MainWindow.hpp
@@ -28,7 +28,7 @@ public:
 
         // Setup Platform/Renderer backends
         ImGui_ImplWin32_Init(Handle());
-        ImGui_ImplDX9_Init(g_pd3dDevice);
+        ImGui_ImplDX9_Init(s_d3ddevice);
     }
 
     virtual void OnCloseWindow()
@@ -42,102 +42,77 @@ public:
 
     virtual void OnDraw()
     {
-        // Our state
-        bool show_demo_window = true;
-        bool show_another_window = false;
-        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-        // Start the Dear ImGui frame
-        ImGui_ImplDX9_NewFrame();
         ImGui_ImplWin32_NewFrame();
+        ImGui_ImplDX9_NewFrame();
+
+
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        //ImGui::Begin("My shapes");
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        ImDrawList* draw_list = ImGui::GetWindowDrawList();
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+        // Get the current ImGui cursor position
+        ImVec2 p = ImGui::GetCursorScreenPos();
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+        // Draw a red circle
+        draw_list->AddCircleFilled(ImVec2(p.x + 50, p.y + 50), 30.0f, IM_COL32(255, 0, 0, 255), 16);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+        // Draw a 3 pixel thick yellow line
+        draw_list->AddLine(ImVec2(p.x, p.y), ImVec2(p.x + 100.0f, p.y + 100.0f), IM_COL32(255, 255, 0, 255), 3.0f);
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+        // Advance the ImGui cursor to claim space in the window (otherwise the window will appears small and needs to be resized)
+        ImGui::Dummy(ImVec2(200, 200));
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        // Rendering
+        //ImGui::End();
         ImGui::EndFrame();
-        g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
-        g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
-        g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+//        s_d3ddevice->SetRenderState(D3DRS_ZENABLE, FALSE);
+//        s_d3ddevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+//        s_d3ddevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
         D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x*clear_color.w*255.0f), (int)(clear_color.y*clear_color.w*255.0f), (int)(clear_color.z*clear_color.w*255.0f), (int)(clear_color.w*255.0f));
-        g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
-        if (g_pd3dDevice->BeginScene() >= 0)
+//        s_d3ddevice->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
+        if (s_d3ddevice->BeginScene() >= 0)
         {
             ImGui::Render();
             ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
-            g_pd3dDevice->EndScene();
+            s_d3ddevice->EndScene();
         }
-        HRESULT result = g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+        HRESULT result = s_d3ddevice->Present(nullptr, nullptr, nullptr, nullptr);
 
         // Handle loss of D3D9 device
-        if (result == D3DERR_DEVICELOST && g_pd3dDevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
-            ResetDevice();
-
+//        if (result == D3DERR_DEVICELOST && s_d3ddevice->TestCooperativeLevel() == D3DERR_DEVICENOTRESET)
+//            ResetDevice();
     }
 
     virtual void OnSize(int width, int height)
     {
-        if (g_pd3dDevice != NULL)
+        if (s_d3ddevice != nullptr)
         {
-            g_d3dpp.BackBufferWidth = width;
-            g_d3dpp.BackBufferHeight = height;
+            s_d3dpp.BackBufferWidth = width;
+            s_d3dpp.BackBufferHeight = height;
             ResetDevice();
         }
-
     }
 
 private:
 
     bool CreateDeviceD3D(HWND hWnd)
     {
-        if ((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == NULL)
+        if ((s_d3d = Direct3DCreate9(D3D_SDK_VERSION)) == nullptr)
             return false;
 
         // Create the D3DDevice
-        ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
-        g_d3dpp.Windowed = TRUE;
-        g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-        g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN; // Need to use an explicit format with alpha if needing per-pixel alpha composition.
-        g_d3dpp.EnableAutoDepthStencil = TRUE;
-        g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
-        g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;           // Present with vsync
-        //g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
-        if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
+        ZeroMemory(&s_d3dpp, sizeof(s_d3dpp));
+        s_d3dpp.Windowed = TRUE;
+        s_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+        s_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN; // Need to use an explicit format with alpha if needing per-pixel alpha composition.
+        s_d3dpp.EnableAutoDepthStencil = TRUE;
+        s_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+        s_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;           // Present with vsync
+        //s_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_IMMEDIATE;   // Present without vsync, maximum unthrottled framerate
+        if (s_d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &s_d3dpp, &s_d3ddevice) < 0)
             return false;
 
         return true;
@@ -145,23 +120,23 @@ private:
 
     void CleanupDeviceD3D()
     {
-        if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
-        if (g_pD3D) { g_pD3D->Release(); g_pD3D = NULL; }
+        if (s_d3ddevice) { s_d3ddevice->Release(); s_d3ddevice = nullptr; }
+        if (s_d3d) { s_d3d->Release(); s_d3d = nullptr; }
     }
 
     void ResetDevice()
     {
         ImGui_ImplDX9_InvalidateDeviceObjects();
-        HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
+        HRESULT hr = s_d3ddevice->Reset(&s_d3dpp);
         if (hr == D3DERR_INVALIDCALL)
             IM_ASSERT(0);
         ImGui_ImplDX9_CreateDeviceObjects();
     }
 
     // Data
-    static inline LPDIRECT3D9              g_pD3D { NULL };
-    static inline LPDIRECT3DDEVICE9        g_pd3dDevice { NULL };
-    static inline D3DPRESENT_PARAMETERS    g_d3dpp;
+    static inline LPDIRECT3D9              s_d3d { nullptr };
+    static inline LPDIRECT3DDEVICE9        s_d3ddevice { nullptr };
+    static inline D3DPRESENT_PARAMETERS    s_d3dpp;
 };//class Window
 
 
